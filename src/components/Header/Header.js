398f11667d855633001212f9a5dbd8ed1985a757
Crystal Tai <crystal.tai@ey.com>
2023-01-30 17:24:02 -0500

    feat: header initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CoffeeIcon from '@mui/icons-material/CoffeeMaker';
+import CartIcon from '@mui/icons-material/ShoppingCart';
+
+function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' elevation='0' sx={{ bgcolor: 'grey.700' }}>
+        <Toolbar>
+          <CoffeeIcon sx={{ mr: 1 }} />
+          <Typography
+            variant='h6'
+            noWrap
+            component='a'
+            href='/'
+            sx={{
+              mr: 2,
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+            }}
+          >
+            Coffee Beans
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box>
+            <IconButton color='inherit'>
+              <Badge badgeContent={1} color='error'>
+                <CartIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
---
+++ src/theme.js
@@ -0,0 +1,62 @@
+import { createTheme } from "@mui/material/styles";
+
+// custom theme for this app
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#5E81AC',
+    },
+    secondary: {
+      main: '#8FBCBB',
+      contrastText: '#ECEFF4',
+    },
+    background: {
+      default: '#ECEFF4',
+    },
+    text: {
+      primary: '#2E3440',
+      secondary: '#3B4252',
+    },
+    error: {
+      main: '#BF616A',
+    },
+    warning: {
+      main: '#EBCB8B',
+    },
+    info: {
+      main: '#88C0D0',
+    },
+    success: {
+      main: '#A3BE8C',
+    },
+    grey: {
+      200: '#E5E9F0',
+      300: '#D8DEE9',
+      400: '#4C566A',
+      500: '#434C5E',
+      600: '#3B4252',
+      700: '#2E3440'
+    },
+    divider: '#3B4252',
+  },
+  text: {
+    primary: '#2E3440',
+    secondary: '#434C5E',
+  },
+  components: {
+    // Buttons
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true, // Disable button shadow
+      }
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true, // Disable ripple effect when clicked
+      },
+    },
+  }
+});
+
+export default theme;
