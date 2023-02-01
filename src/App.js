b2d7bd89315943a57133be52f57d8af6386d8110
Crystal Tai <crystal.tai@ey.com>
2023-02-01 17:34:26 -0500

    feat: center align grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
 import Header from './components/Header/Header';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Product from './components/Product/Product';
 import coffeeData from './constants';
@@ -13,10 +14,11 @@ function App() {
       <CssBaseline />
       <div className='App'>
         <Header />
-        <Box sx={{ flexGrow: 1, padding: '50px' }}>
-          <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Container>
+        <Box sx={{ padding: '30px', marginTop: '30px' }}>
+          <Grid container spacing={{ xs: 4, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             {coffeeData.map((coffee, index) => (
-              <Grid item xs={4} sm={4} md={4} key={index}>
+              <Grid item xs={4} sm={4} md={4} key={index} sx={{ padding: '0' }}>
                 <Product
                   key={coffee.name}
                   name={coffee.name}
@@ -28,6 +30,7 @@ function App() {
             ))}
           </Grid>
         </Box>
+        </Container>
       </div>
     </ThemeProvider>
   );
