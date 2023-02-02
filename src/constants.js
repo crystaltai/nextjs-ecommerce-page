d7ff15119982182ab6a1fd5b0ad1b319eb98ce28
Crystal Tai <crystaltai315@gmail.com>
2023-02-02 10:50:48 -0500

    feat: add additional product info to products object

--- src/constants.js
+++ src/constants.js
@@ -11,36 +11,99 @@ const coffeeData = [
     notes: ['Honeydew', 'Apricot', 'Hazelnut'],
     price: 18.25,
     picture: CoffeeBag1,
+    details: {
+      type: 'Single Origin',
+      roast: 'Light',
+      origin: 'Ethiopia',
+    },
   },
   {
     name: 'Streetlevel',
     notes: ['Clementine', 'Red Apple', 'Honeycomb'],
     price: 18.75,
     picture: CoffeeBag2,
+    details: {
+      type: 'Blend',
+      roast: 'Light',
+      region: 'Colombia',
+    },
   },
   {
     name: 'Sermon',
     notes: ['Blueberry Pie', 'Cocoa', 'Candied Pecan'],
     price: 18.75,
     picture: CoffeeBag3,
+    details: {
+      type: 'Single Origin',
+      roast: 'Medium',
+      region: 'Ethiopia',
+    },
   },
   {
     name: 'The 1950',
     notes: ['Nectarine', 'Key Lime', 'Brown Sugar'],
     price: 19.5,
     picture: CoffeeBag4,
+    details: {
+      type: 'Decaf',
+      roast: 'Light',
+      region: 'Guatemala',
+    },
   },
   {
     name: 'Santa Clara',
     notes: ['Green Apple', 'Melon', 'Caramel'],
     price: 21.25,
     picture: CoffeeBag5,
+    details: {
+      type: 'Blend',
+      roast: 'Dark',
+      region: 'Panama',
+    },
   },
   {
     name: 'Elvis Tineo',
     notes: ['Toasted Coconut', 'Cherry', 'Dark Chocolate'],
     price: 22.75,
     picture: CoffeeBag6,
+    details: {
+      type: 'Single Origin',
+      roast: 'Medium',
+      region: 'Costa Rica',
+    },
+  },
+  {
+    name: 'French Blue Blend',
+    notes: ['Apple', 'Pear', 'Chocolate'],
+    price: 18.5,
+    picture: CoffeeBag1,
+    details: {
+      type: 'Decaf',
+      roast: 'Medium',
+      origin: 'Colombia',
+    },
+  },
+  {
+    name: 'Windowlevel',
+    notes: ['Orange', 'Watermelon', 'Honeycomb'],
+    price: 16.75,
+    picture: CoffeeBag2,
+    details: {
+      type: 'Single Origin',
+      roast: 'Light',
+      region: 'Ethiopia',
+    },
+  },
+  {
+    name: 'Windy Road',
+    notes: ['Pumpkin', 'Cinnamon', 'Hazelnut'],
+    price: 18.75,
+    picture: CoffeeBag3,
+    details: {
+      type: 'Blend',
+      roast: 'Medium',
+      region: 'Guatemala',
+    },
   },
 ];
 
