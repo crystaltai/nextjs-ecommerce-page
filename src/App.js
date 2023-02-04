c75ef61b5b7ace34e615165f4f82d7ea74034224
Crystal Tai <crystaltai315@gmail.com>
2023-02-03 23:52:06 -0500

    feat: initial commit for left sidebar

--- src/App.js
+++ src/App.js
@@ -7,30 +7,40 @@ import Grid from '@mui/material/Grid';
 import Product from './components/Product/Product';
 import coffeeData from './constants';
 import './App.css';
+import FilterBar from './components/FilterBar/FilterBar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className='App'>
+        {/* Header */}
         <Header />
-        <Container>
-        <Box sx={{ padding: '30px', marginTop: '30px' }}>
-          <Grid container spacing={{ xs: 4, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            {coffeeData.map((coffee, index) => (
-              <Grid item xs={4} sm={4} md={4} key={index} sx={{ padding: '0' }}>
-                <Product
-                  key={coffee.name}
-                  name={coffee.name}
-                  notes={coffee.notes}
-                  price={coffee.price}
-                  picture={coffee.picture}
-                />
+        {/* Main */}
+        <Box sx={{ display: 'flex' }}>
+          {/* Filter Section */}
+          <Box sx={{ paddingLeft: '20px', paddingRight: '20px' }}>
+            <FilterBar />
+          </Box>
+          {/* Product Listings */}
+          <Box sx={{ flexGrow: 2 }}>
+            <Container maxWidth='lg'>
+              <Grid container sx={{}} spacing={{ xs: 3, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {coffeeData.map((coffee, index) => (
+                  <Grid item xs={8} sm={4} md={4} key={index} sx={{ padding: '0' }}>
+                    <Product
+                      key={coffee.name}
+                      name={coffee.name}
+                      notes={coffee.notes}
+                      price={coffee.price}
+                      picture={coffee.picture}
+                    />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
+            </Container>
+          </Box>
         </Box>
-        </Container>
       </div>
     </ThemeProvider>
   );
---
+++ src/components/FilterBar/FilterBar.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import Box from '@mui/system/Box';
+
+function FilterBar() {
+  return (
+    <Box>
+      <Typography>Find your bag</Typography>
+    </Box>
+  );
+}
+
+export default FilterBar;
